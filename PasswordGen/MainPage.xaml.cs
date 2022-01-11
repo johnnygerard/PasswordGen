f279b40cd4b79ca628b81c29ac0e75da931aabc7
Johnny Gérard <contact@johnnygerard.dev>
2022-01-11 01:36:49 +0100

    Refactor MainPage

--- PasswordGen/MainPage.xaml.cs
+++ PasswordGen/MainPage.xaml.cs
@@ -10,9 +10,6 @@
 
     using MUXC = Microsoft.UI.Xaml.Controls;
 
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         private readonly MUXC.NavigationViewItem _startPage;
