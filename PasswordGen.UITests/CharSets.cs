1e57a5159f1ff27a5d80ed9376217f4e1dce739d
Johnny Gérard <contact@johnnygerard.dev>
2022-02-03 18:13:07 +0100

    Change local 'ascii' into internal field

--- PasswordGen.UITests/CharSets.cs
+++ PasswordGen.UITests/CharSets.cs
@@ -9,15 +9,15 @@
         internal static readonly string _lowercase;
         internal static readonly string _uppercase;
         internal static readonly string _symbols;
+        internal static readonly string _ascii;
 
         static Charsets()
         {
-            string ascii = BuildCharset('!', '~');
-
+            _ascii = BuildCharset('!', '~');
             _digits = BuildCharset('0', '9');
             _lowercase = BuildCharset('a', 'z');
             _uppercase = BuildCharset('A', 'Z');
-            _symbols = string.Join(null, ascii.Except(_digits).Except(_lowercase).Except(_uppercase));
+            _symbols = string.Join(null, _ascii.Except(_digits).Except(_lowercase).Except(_uppercase));
         }
 
         /// <summary>
